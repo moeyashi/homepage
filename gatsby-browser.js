c9c5f303397178f5a0c5be6cdff45e138c1f4999
moeyashi <yorsr315@yahoo.co.jp>
2020-10-06 22:32:39 +0900

    add nprogress

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+import NProgress from "nprogress"
 import "highlight.js/styles/github.css"
+import "./src/browser.css"
+
+export const onClientEntry = () => {
+  NProgress.configure({ showSpinner: false })
+}
+
+export const onPreRouteUpdate = () => {
+  NProgress.start()
+}
+
+export const onRouteUpdate = () => {
+  NProgress.done()
+}
